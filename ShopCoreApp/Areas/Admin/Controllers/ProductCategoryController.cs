a580c500af0f08b234d937dac56831100022ec38
Admin <Admin@DESKTOP-JFSJJDK>
2018-03-18 17:48:10 +0700

    get category by treeview

---
+++ ShopCoreApp/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShopCoreApp.Application.Interfaces;
+
+namespace ShopCoreApp.Areas.Admin.Controllers
+{
+    public class ProductCategoryController : BaseController
+    {
+        public IProductCategoryService _productCategoryService;
+        public ProductCategoryController( IProductCategoryService productCategoryService)
+        {         
+            _productCategoryService = productCategoryService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region Get Data API
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var model = _productCategoryService.GetAll();
+            return new OkObjectResult(model);
+        }
+        #endregion
+    }
+}
